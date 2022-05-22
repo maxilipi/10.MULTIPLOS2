0f0904a445fc59166eb72ffd03f58a82c68b018e
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-05-22 20:05:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+let numero1 : number = prompt ("cual es el numero1");
+let numero2 : number = prompt ("cual es el numero2");
+
+
+{
+  function multiplo (numero1,numero2) {
+
+let resultado = numero1%numero2;
+if (resultado===0)
+  console.log (false);
+else
+  console.log(true);
+}
+ 
+
+console.log multiplo(); 
